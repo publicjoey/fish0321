80a5c9a8962d98487ccc1f0c94de1141462073ad
joey <jobjoey2021@gmail.com>
2025-03-21 14:58:57 +0800

    click add fish_ok
    滑鼠點擊位置新增魚

--- Aquarium.gd
+++ Aquarium.gd
@@ -6,15 +6,25 @@ extends Node2D
 func _ready():
 	randomize()
 	_spawn_fish()
+	
+	# 讓 Godot 偵測滑鼠點擊
+	set_process_input(true)
 
 func _spawn_fish():
 	var aquarium_size = get_viewport_rect().size  # 取得水族箱大小
 
 	for i in range(fish_count):
-		var fish = fish_scene.instantiate()  # 建立魚
-		fish.position = Vector2(
+		_add_fish(Vector2(
 			randf_range(50, aquarium_size.x - 50),
 			randf_range(50, aquarium_size.y - 50)
-		)  # 隨機位置
+		))  # 隨機位置
 
-		get_node("FishContainer").add_child(fish)  # 加入場景
+func _add_fish(position: Vector2):
+	var fish = fish_scene.instantiate()  # 建立魚
+	fish.position = position  # 設定位置
+	get_node("FishContainer").add_child(fish)  # 加入場景
+
+# 處理滑鼠點擊
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		_add_fish(event.position)  # 在滑鼠點擊位置新增魚
